d0baf71b33e866d2bb22216436d8916d175fedf2
Matěj Schrödl <msamot@seznam.cz>
2022-09-30 10:56:44 +0200

    understanding tga parsing

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ const TGAColor red   = TGAColor(255, 0,   0,   255);
 
 int main(int argc, char** argv) {
 	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(52, 41, red);
+	image.set(70, 70, red);
+	image.set(50,34,white);
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	
-	getchar();
 	return 0;
 }
 
