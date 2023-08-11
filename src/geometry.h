e9816086569d0c6b2658c85d4cb8124003748822
schrodlm <msamot@seznam.cz>
2023-08-11 10:11:42 +0200

    vector definitions

--- src/geometry.h
+++ src/geometry.h
@@ -1,7 +1,9 @@
 #ifndef __GEOMETRY_H__
 #define __GEOMETRY_H__
 
+#include <cmath>
 #include <iostream>
+#include <ostream>
 
 template <typename T> struct Vec2 {
   union {
@@ -23,12 +25,71 @@ template <typename T> struct Vec2 {
   inline Vec2<T> operator-(const Vec2<T> &V) const {
     return Vec2<T>{u - V.u, v + V.v};
   }
-  inline Vec2<T> operator*(float f) const { return Vec2<T>{u * f, v * f}; }
+  inline Vec2<T> operator*(const float f) const {
+    return Vec2<T>{u * f, v * f};
+  }
+
   template <typename>
   friend std::ostream &operator<<(std::ostream &s, Vec2<T> &V);
 };
 
-template <typename T> struct Vec3 {};
+template <typename T> struct Vec3 {
+  union {
+    struct {
+      T x, y, z;
+    };
+    struct {
+      T vert, uv, norm;
+    };
+    T raw[3];
+  };
+
+  Vec3() : x(0), y(0), z(0) {}
+  Vec3(T _x, T _y, T _z) : x(_x), y(_y), z(_z) {}
+
+  // cross product
+  inline Vec3<T> operator^(const Vec3<T> &V) const {
+    return Vec3<T>{y * V.z - z * V.y, z * V.x - x * V.z, x * V.y - y * V.x};
+  }
+  inline Vec3<T> operator+(const Vec3<T> &V) const {
+    return Vec3<T>{x + V.x, y + V.y, z + V.z};
+  }
+  inline Vec3<T> operator-(const Vec3<T> &V) const {
+    return Vec3<T>{x - V.x, y - V.y, z - V.z};
+  }
+  inline Vec3<T> operator*(const float f) const {
+    return Vec3<T>{x * f, y * f, z * f};
+  }
+  inline T operator*(const Vec3<T> &V) const {
+    return x * V.x + y * V.y + z * V.z;
+  }
+
+  float length() { return std::sqrt(x * x + y * y + z * z); }
+
+  Vec3<T> &normalize(T l = 1) {
+    *this = (*this) * (l / length());
+    return *this;
+  }
+
+  template <typename>
+  friend std::ostream &operator<<(std::ostream &out, const Vec3<T> &V);
+};
+
+typedef Vec2<float> Vec2f;
+typedef Vec2<int> Vec2i;
+typedef Vec3<float> Vec3f;
+typedef Vec3<int> Vec3i;
+
+template <typename T>
+std::ostream &operator<<(std::ostream &out, const Vec2<T> &V) {
+  out << "(" << V.x << "," << V.y << ")" << std::endl;
+  return out;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &out, const Vec3<T> &V) {
+  out << "(" << V.x << "," << V.y << "," << V.z << ")" << std::endl;
+  return out;
+}
 
 #endif // !__GEOMETRY_H__
-#define __GEOMETRY_H__
---
+++ src/model.h
@@ -0,0 +1,6 @@
+#ifndef __MODEL_H__
+#define __MODEL_H__
+
+class Model {};
+
+#endif // !__MODEL_H__
