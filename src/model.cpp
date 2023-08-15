d3d2cca7cc1d46def61b181c694dfc774f8feaac
schrodlm <msamot@seznam.cz>
2023-08-15 16:16:12 +0200

    Model implemntation

---
+++ src/model.cpp
@@ -0,0 +1,75 @@
+#include "model.h"
+#include <fstream>
+#include <string>
+#include <sstream>
+
+// parses wevafront fileformat
+Model::Model(const char *filename)
+{
+
+  std::ifstream in;
+
+  in.open(filename, std::ios::in);
+
+  // check if open was succesful
+  if (in.fail())
+  {
+    std::cerr << "Can't open file " << filename << "\n";
+    in.close();
+    return;
+  }
+
+  std::string line;
+  while (!in.eof())
+  {
+    std::getline(in, line);
+    std::istringstream iss(line.c_str());
+    char trash;
+
+    // line contains vertex information
+    if (line.compare(0, 2, "v "))
+    {
+      iss >> trash;
+      Vec3f v;
+      iss >> v.x >> v.y >> v.z;
+      verts_.push_back(v);
+    }
+
+    else if (line.compare(0, 2, "f "))
+    {
+      std::vector<int> f;
+      int itrash, idx;
+      std::vector<int> v;
+
+      iss >> trash;
+      while (iss >> idx >> trash >> itrash >> trash >> itrash)
+      {
+        // indexes in wavefront ff start from 1
+        f.push_back(idx - 1);
+      }
+      faces_.push_back(f);
+    }
+  }
+  std::cerr << "# v# " << verts_.size() << " f# " << faces_.size() << std::endl;
+}
+
+int Model::nverts(){
+  return static_cast<int>(verts_.size());
+}
+
+int Model::nfaces(){
+  return static_cast<int>(faces_.size());
+}
+
+Vec3f Model::vert(int i){
+  if(i < 0 || i >= nverts()) throw std::out_of_range("Index out of bounds");
+  return verts_[i];
+}
+
+std::vector<int> Model::face(int idx){
+  if(idx < 0 || idx >= nfaces()) throw std::out_of_range("Index out of bounds");
+  return faces_[idx];
+}
+
+
+
--- src/model.h
+++ src/model.h
@@ -1,6 +1,24 @@
 #ifndef __MODEL_H__
 #define __MODEL_H__
 
-class Model {};
+#include "geometry.h"
+#include <vector>
+
+class Model {
+
+private:
+  std::vector<Vec3f> verts_;
+  std::vector<std::vector<int>> faces_;
+
+public:
+  Model(const char *filename);
+  ~Model() = default;
+
+  int nverts();
+  int nfaces();
+
+  Vec3f vert(int i);
+  std::vector<int> face(int idx);
+};
 
 #endif // !__MODEL_H__
